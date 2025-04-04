d7399f233d927625a3ffa41353919af6422963f7
Bubbq <soccer17nemo@gmail.com>
2025-04-04 12:04:57 -0500

    created generic list, split filepath by invalid linux characters to recombine with appropraite escape sequences

--- music_player.c
+++ music_player.c
@@ -3,9 +3,8 @@
 #include "unistd.h"
 #include "string.h"
 #include "headers/raylib.h"
-#include <stdio.h>
 
-#define MAX_LEN 256
+#define MAX_LEN 512
 #define N_PLAYLIST 10
 const char* MUSIC_PATH = "$HOME/Music";
 
@@ -14,26 +13,171 @@ typedef struct
     char file_path[MAX_LEN];
 }Playlist;
 
-// typedef struct
-// {
-//     Playlist* list;
-//     int current_playlist_index;
-// }Library;
+typedef struct
+{
+    int length;
+    char* value;
+} string;
+
+string create_string(char* str)
+{
+    string string;
+    string.length = strlen(str);
+    string.value = malloc(sizeof(char) * MAX_LEN);
+    strcpy(string.value, str);
+    
+    return string;
+}
+
+void update_string(string* str, char* new_str)
+{
+    if(new_str == NULL)
+        return;
+    else if(sizeof(new_str) > (sizeof(char) * MAX_LEN))
+    {
+        perror("new string is larger than the string struct's max size");
+        return;
+    }
+    
+    strcpy(str->value, new_str);
+    str->length = strlen(str->value);
+}
+
+int get_string_length(string* str)
+{
+    str->length = strlen(str->value);
+    return str->length;
+}
+
+typedef struct
+{
+    int size;
+    void* elements;
+    size_t mem_capacity;
+} list;
+
+size_t get_size(char type)
+{
+    switch (type)
+    {
+        case 's': 
+            return sizeof(string); 
+            break;
+        case 'i':
+            return sizeof(int);
+        default: 
+            return 0;
+    }
+}
+
+list create_list(char type)
+{
+    list list;
+    list.size = 0;
+
+    list.mem_capacity = get_size(type);
+    list.elements = malloc(list.mem_capacity);
+
+    return list;
+}
+
+void resize_list(list* list)
+{
+    list->mem_capacity *= 2;
+    list->elements = realloc(list->elements, list->mem_capacity);
+}
+
+void add_list_element(list* list, void* input, char type)
+{
+    if((list->size * get_size(type)) == list->mem_capacity)
+    {
+        resize_list(list);
+    }
+
+    void* ptr;
+    switch (type)
+    {
+        case 's': 
+            ptr = ((string*)list->elements) + list->size++;
+            memcpy(ptr, (string*)input, sizeof(string));
+            break;
+        case 'i':
+            ptr = ((int*)list->elements) + list->size++;
+            memcpy(ptr, (int*)input, get_size(type));
+        default:
+            return;
+    }
+}
+
+void print_list(list list, char type)
+{
+    for(int i = 0; i < list.size; i++)
+    {
+        switch (type)
+        {
+            case 's': 
+                printf("%s\n", ((string*)list.elements)[i].value);
+                break;
+            case 'i':
+                printf("%d\n", ((int*)list.elements)[i]);
+                break;
+            default:
+                return;
+        }
+    }
+}
+
+void free_list(list* list)
+{
+    free(list->elements);
+}
 
 char* get_linux_format(char file_path[MAX_LEN])
 {
-    int n = 7;
-    char* bad_character[] = {" ", "(", ")", "&", "'", "`", "\\"};
-    char* replacement_character[] = {"\\ ", "\\(", "\\)", "\\&", "\\'", "\\`", "\\\""};
-    char* copy = file_path;
-    
-    // TODO: convert string into linux format
-    
-    
-    return copy;
+    const char* DELIM = " ()&'`\\";
+    char* replacement_characters[] = {"\\ ", "\\(", "\\)", "\\&", "\\'", "\\`", "\\\""};
+
+    list valid_path_chunks = create_list('s');
+    list replacements = create_list('i');
+    string str = create_string("");
+
+    // get a list of replacement chars to be appended with the valid path parts
+    for(int i = 0; i < strlen(file_path); i++)
+    {
+        char c = file_path[i];
+
+        for(int j = 0; j < strlen(DELIM); j++)
+        {
+            if(c == DELIM[j])
+            {
+                int replacement_index = j;
+                add_list_element(&replacements, &replacement_index, 'i');
+            }
+        }
+    }
+    // print_list(replacements, 'i');
+
+    // get list of valid chunks of the filepath TODO: fix string value storege, its taking all into one string?
+    char* buffer = strtok(file_path, DELIM);   
+
+    while(buffer != NULL)
+    {
+        update_string(&str, buffer);
+        add_list_element(&valid_path_chunks, &str, 's');
+        buffer = strtok(NULL, DELIM);
+    }
+    // printf("%d\n", valid_path_chunks.size);
+    // print_list(valid_path_chunks, 's');
+
+    // dealloc
+    free_list(&valid_path_chunks);
+    free_list(&replacements);
+    free(str.value);
+
+    return file_path;
 }
 
-void get_playlist_paths(Playlist* playlists)
+void get_playlist_paths(Playlist playlists)
 {
     char current_directory[MAX_LEN];
     if(getcwd(current_directory, sizeof(current_directory)) == NULL)
@@ -54,9 +198,8 @@ void get_playlist_paths(Playlist* playlists)
     {
         int stopping_point = strcspn(buffer, "\n");
         buffer[stopping_point] = '\0';
-     
-        strcpy(playlists[count].file_path, buffer);
-        printf("%s\n", playlists[count].file_path);
+        strcpy(playlists.file_path, get_linux_format(buffer));
+        // printf("%s\n", playlists[count].file_path);
         count++;
     }
 
@@ -65,18 +208,6 @@ void get_playlist_paths(Playlist* playlists)
 
 int main()
 {
-    Playlist playlist[N_PLAYLIST];
-    // SetTraceLogLevel(LOG_ERROR);
-    // InitWindow(1000, 1000, "music_player");
-    // SetTargetFPS(60);
-    get_playlist_paths(playlist);
-    // while(!WindowShouldClose())
-    // {
-    //     BeginDrawing();
-    //         DrawFPS(0, 0);
-    //         ClearBackground(BLACK);
-    //     EndDrawing();
-    // }
-    // CloseWindow();
-    return 0;
+    Playlist playlists;
+    get_playlist_paths(playlists);
 }
